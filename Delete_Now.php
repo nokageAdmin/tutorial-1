39744c660ea4d0b8419b9280023f7c2635da604a
Malicsuave <reywillardd01@gmail.com>
2024-09-06 16:17:44 +0800

    init

--- Delete_Now.php
+++ Delete_Now.php
@@ -8,7 +8,7 @@ $user_id = $_POST["user_id"];
 
 mysqli_query($connections, "DELETE FROM mytbl WHERE id = '$user_id' ");
 
-echo "<script language='javascript'>alert('Record has been Deleted!')</script>";
+echo "<script language='javascript'>alert('Record has been deleted!')</script>";
 
 echo "<script>window.location.href='index.php';</script>";
 
